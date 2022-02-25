4fab6f65f04ba06c78d9c6c59b159eb5e500bcdf
manik1104 <100420934+manik1104@users.noreply.github.com>
2022-02-25 20:30:42 +0530

    InitailCheckin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
+const initialValues = {
+  zipCodeFrom: "",
+  zipCodeTo: "",
+  units: "",
+  message: "",
+};
+
 
 function App() {
+
+  const [values, setValues] = useState(initialValues);
+  const [message, setMessage] = useState(false);
+  const [response, setResponse] = useState({});
+  
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        fromZipCode: values.zipCodeFrom,
+        toZipCode: values.zipCodeTo
+      })
+    };
+    fetch('https://localhost:44315/GeoLocation/GetPostalCodeDetails', requestOptions)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+
+        setResponse(response);
+        setMessage(true);
+        window.setTimeout(() => {
+          setMessage(false);
+        }, 10000)
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container middle-content col-md-10">
+      <div className="card">
+        <div className="card-body">
+          <form>
+            <div className="row">
+              <div className="form-group col-md-4">
+                <label for="zipCodeFrom">Zip Code From</label>
+                <input type="text" className="form-control" value={values.zipCodeFrom}
+                  onChange={handleInputChange} name="zipCodeFrom" placeholder="Zip Code From" />
+              </div>
+              <div className="form-group col-md-4">
+                <label for="zipCodeTo">Zip Code To</label>
+                <input type="text" className="form-control" value={values.zipCodeTo}
+                  onChange={handleInputChange} name="zipCodeTo" placeholder="Zip Code To" />
+              </div>
+
+            </div>
+            <button type="button" onClick={handleSubmit} className="seperate btn btn-primary">Submit</button>
+          </form>
+        </div>
+        {message ? <AlertMessage data={response} /> : ""}
+      </div>
     </div>
   );
 }
+const AlertMessage = props => {
+  return <div className="alert alert-success" role="alert">
+    <p>The distance between {props.data.FromCity} and {props.data.ToCity} is {props.data.Distance} mi</p>
 
+  </div>
+};
 export default App;
